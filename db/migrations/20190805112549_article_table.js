82bce27d31b8f0b8e93e1625390c625dc5c12b2d
Joe <radiob0x101@gmail.com>
2019-08-06 11:56:59 +0100

    finished seeding and fixed some migration errors

--- db/migrations/20190805112549_article_table.js
+++ db/migrations/20190805112549_article_table.js
@@ -3,10 +3,10 @@ exports.up = function(knex) {
   return knex.schema.createTable('articles', articlesTable => {
     articlesTable.increments('article_id').primary();
     articlesTable.string('title');
-    articlesTable.string('body');
+    articlesTable.text('body');
     articlesTable.integer('votes').defaultTo(0);
-    articlesTable.string('topics').references('topics.slug');
-    articlesTable.string('authors').references('users.username');
+    articlesTable.string('topic').references('topics.slug');
+    articlesTable.string('author').references('users.username');
     articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
--- db/migrations/20190805112555_comment_table.js
+++ db/migrations/20190805112555_comment_table.js
@@ -2,10 +2,11 @@ exports.up = function(knex) {
   console.log('Creating the Comments table..');
   return knex.schema.createTable('comments', commentsTable => {
     commentsTable.increments('comment_id').primary();
+    commentsTable.text('author').references('users.username');
     commentsTable.integer('article_id').references('articles.article_id');
     commentsTable.integer('votes').defaultTo(0);
     commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
-    commentsTable.string('body');
+    commentsTable.text('body');
   });
 };
 
